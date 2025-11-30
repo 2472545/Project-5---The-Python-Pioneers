c4b256408388eb55613326c512c65139d1262d4f
2472545 <2472545@edu.vaniercollege.qc.ca>
2025-11-29 22:20:53 -0500

    Create filter_signal.py

---
+++ file_handling_functions.py/filter_signal.py
@@ -0,0 +1,9 @@
+WINDOW_SIZE = 15  
+def moving_average(signal, window = WINDOW_SIZE):
+    kernel = np.ones(window) / window
+    return np.convolve(signal, kernel, mode = 'same')
+
+def filter_signal(df, window_size=WINDOW_SIZE):
+    df_filtered = df.copy()
+    df_filtered['Filtered Voltage'] = (df_filtered.groupby('Patient ID')['Voltage'].transform(lambda x: moving_average(x, window=window_size))
+    return df_filtered
