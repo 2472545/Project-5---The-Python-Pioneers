bd9401b20a5eba14c9e5b6504d26882986d33d63
2472545 <2472545@edu.vaniercollege.qc.ca>
2025-11-29 22:15:06 -0500

    Update Raw_ECG_Plot.py

--- animations_and_graphs/Raw_ECG_Plot.py
+++ animations_and_graphs/Raw_ECG_Plot.py
@@ -1,1 +1,7 @@
-
+plt.figure(figsize=(14,8))
+    sns.lineplot(data=df,  x= 'Time', y="Voltage", hue="Patient ID", palette = 'rainbow')
+    plt.title("Voltage vs. Time")
+    plt.savefig(r"C:\Users\gabri\OneDrive\Desktop\raw_ecg.png") # Saving in png format within the file path
+    plt.xlim(0, 10)
+    plt.show()
+    plt.close()
