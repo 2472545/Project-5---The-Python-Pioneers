f274d91e86b01dc54e40a1d5c00864e4993ce5ff
AndrewCreese <6322964@edu.vaniercollege.qc.ca>
2025-12-01 12:40:33 -0500

    Update Andrew's code.py
    The R-Peak detection works!!

--- Andrew's code.py
+++ Andrew's code.py
@@ -3,8 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib import animation
-
-DATAFILE = r"K:\Science and Music Double DCS Program\Science\Programming in Science\Project\ECG_DATA.csv" # Replace this with the filename from your computer
+DATAFILE = r"D:\Science and Music Double DCS Program\Science\Programming in Science\Project\ECG_DATA.csv" # Replace this with the filename from your computer
 FS = 400 # Sampling frequency in Hz
 WINDOW_SIZE = 15 # For moving average calculation
 
@@ -21,25 +20,29 @@ def filter_signal(df, window = WINDOW_SIZE):
         )
     return df
 
-def detect_r_peaks(df, threshold_percentage=0.1):
-    r_peak_results = []
+def detect_r_peaks(df, threshold_percentage=0.5):
+    all_peaks = {}
     for patient, group in df.groupby('Patient ID'):
+        group = group[group['Time'] > 0.2] # Excluding the first 0.2 seconds of data since it is not representative
         ecg = group['Voltage'].values
         time = group['Time'].values
-        threshold = threshold_percentage * np.max(ecg)
-        peak_times = []
+        baseline = np.median(ecg) # Computing baseline for this patient
+        peak_height = np.max(ecg) - baseline # Computing peak height for this patient
+        threshold = threshold_percentage * peak_height # Computing threshold for this patient
+        peak_times = [] # Initiating an empty array to store peak times
         for i in range(1, len(ecg) - 1):
-            if ecg[i] > threshold and ecg[i] > ecg[i-1] and ecg[i] > ecg[i+1]:
-                peak_times.append(time[i])
-        print(f"Patient {patient} R-Peaks: {peak_times}")
-        # Something is wrong here since the peak times are displayed as empty arrays...
- 
-def compute_metrics(df): # Based on the detect_r_peaks(df, threshold_percentage=0.1) function
-    time = 1 / FS
+            if (ecg[i] - baseline) > threshold and ecg[i] >= ecg[i-1] and ecg[i] > ecg[i+1]:
+                peak_times.append(float(time[i]))
+            all_peaks[patient] = peak_times  # Storing the peak times for this patient
+        print(f"Patient {patient} R-Peaks times (in seconds): {peak_times}") 
+    return all_peaks
+
+def compute_metrics(df): # Based on the detect_r_peaks() function
     BPM_heart_rate = '''60*(number of beats in 10 sec - 1)/(time of last peak - time of first peak in seconds)'''
     rr = '''an array containing the time between each pair of r-peaks'''
     hrv = np.stdv([rr])
     print(f"The BPM heart rate is: {BPM_heart_rate}, \n, The heart rate variability: {hrv}")
+    # TODO
 
 def create_plots(df):
     # Raw ECG Plot
@@ -47,7 +50,7 @@ def create_plots(df):
     sns.lineplot(data=df,  x= 'Time', y="Voltage", hue="Patient ID", palette = 'gist_rainbow')
     sns.hls_palette()
     plt.title("Raw ECG Signals")
-    plt.savefig(r"K:\Science and Music Double DCS Program\Science\Programming in Science\Project\raw_ecg.png") # Replace this with the filename from your computer
+    plt.savefig(r"D:\Science and Music Double DCS Program\Science\Programming in Science\Project\raw_ecg.png") # Replace this with the filename from your computer
     plt.xlim(0, 10)
     plt.show()
     plt.close()
@@ -59,7 +62,7 @@ def create_plots(df):
     plt.xlabel("Time (s)")
     plt.ylabel("Voltage (mV)")
     plt.xlim(df['Time'].min(), df['Time'].min() + 10)  # safer limit
-    plt.savefig(r"K:\Science and Music Double DCS Program\Science\Programming in Science\Project\filtered_ecg.png") # Replace this with the filename from your computer
+    plt.savefig(r"D:\Science and Music Double DCS Program\Science\Programming in Science\Project\filtered_ecg.png") # Replace this with the filename from your computer
     plt.show()
     plt.close()
 
