46d6ae72b47909fcfb849f824ec83efe904cca49
AndrewCreese <6322964@edu.vaniercollege.qc.ca>
2025-12-01 20:42:47 -0500

    Update compute_metrics.py

--- R_peaks_and_compute_metrics/compute_metrics.py
+++ R_peaks_and_compute_metrics/compute_metrics.py
@@ -1,1 +1,11 @@
-# TODO
+def compute_metrics(all_peaks):
+    metrics = []
+    for patient, peak_times in all_peaks.items():
+        bpm = 60 * (len(peak_times) - 1) / (peak_times[-1] - peak_times[0]) # 60*(number of beats in 10 sec - 1)/(time of last peak - time of first peak in seconds)
+        rr = (np.diff(peak_times)) # Computes RR-intervals as the time difference between each pair of R-peaks
+        hrv = np.std(rr) # Standard deviation of RR-intervals
+        patient_metrics = [patient, bpm, rr, hrv]
+        metrics.append(patient_metrics)
+        print(f"Patient {patient}: BPM = {bpm:.2f}, HRV = {hrv:.4f}")
+    return metrics
+    
