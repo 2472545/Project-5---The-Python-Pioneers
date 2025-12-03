dc73515a5afd2bbe6004fcb75e7c8904c45859d3
2472545 <2472545@edu.vaniercollege.qc.ca>
2025-12-02 19:36:59 -0500

    Update RR_scatter.py

--- animations_and_graphs/RR_scatter.py
+++ animations_and_graphs/RR_scatter.py
@@ -1,1 +1,7 @@
-# TODO
+plt.figure(figsize=(6,4))
+    sns.scatterplot(data=df, x="pH", y="Turbidity_NTU", hue="SiteID")
+    plt.title("pH vs Turbidity")
+    plt.savefig(r"C:\Users\azharh\Desktop\ListofFinalProjects\WaterQuality_Project\scatter_pH_vs_turbidity.png") # PUT YOUR PATH
+    plt.close()
+
+# REPLACE WITH TITLE AND DATA ACCORDING TO OUR PROJECT
